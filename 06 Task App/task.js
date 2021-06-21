535c46137f579a73378305c61bfbb2cfffa223ca
tofuLove <vioneverforget@gmail.com>
2021-06-21 15:08:31 +0800

    completed-6th

--- 06 Task App/task.js
+++ 06 Task App/task.js
@@ -32,7 +32,7 @@
         updateTable();
     }
 
-    const updateTable = (newData) => {
+    const updateTable = () => {
         let db = localStorage.getItem("tasks-at-hand");
         let dbParsed = JSON.parse(db);
         table.innerHTML = `
@@ -48,10 +48,10 @@
             const createRow = document.createElement("tr");
             createRow.classList.add("work")
             createRow.innerHTML = `
-            <td><input type="checkbox" id="check${i}" onclick="completed(${i})"></td>
+            <td><input type="checkbox" class="checkbox" id="check${i}" onclick="completed(${i})"></td>
             <td>${dbParsed[i]["task"]}</td>
             <td>${dbParsed[i]["date"]}</td>
-            <td><button onclick="deleteTask(${i})">Delete</button><button class="edit edit-button" onclick="editTask(${i})">Edit</button></td>
+            <td><button class="delete-btn" onclick="deleteTask(${i})">Delete</button><button class="edit-btn" onclick="editTask(${i})">Edit</button></td>
             `;
 
             table.appendChild(createRow);
@@ -98,18 +98,21 @@
 
         let row = document.querySelectorAll(".work");
         row[num].innerHTML = `
-            <td><input type="checkbox" id="check${num}" onclick="completed(${num})"></td>
+            <td><input type="checkbox" class="checkbox" id="check${num}" onclick="completed(${num})"></td>
             <td><input type="text" id="task${num}" placeholder="${dbParsed[num]["task"]}"></td>
             <td><input type="text" id="date${num}" placeholder="${dbParsed[num]["date"]}"></td>
-            <td><button onclick="deleteTask(${num})">Delete</button><input class="submit-btn edit-button" type="submit"></td>
+            <td><input class="submit-btn edit-button" type="submit"></td>
         `;
 
         let submit = document.querySelector(".submit-btn");
+        submit.addEventListener("click", () => {
+            editedData(num)
+        })
         window.addEventListener("keydown", (e) => {
             if (submit !== null && e.key === "Enter") {
-                console.log("works")
+                editedData(num);
             }
-        })
+        });
     }
 
     window.addEventListener("load", updateTable)
@@ -120,9 +123,21 @@
     })
 
     function editedData(num) {
-        let editTask = document.getElementById(`tasks${num}`);
-        let editDate = document.getElementById(`date${num}`);
+        let db = localStorage.getItem("tasks-at-hand");
+        let dbParsed = JSON.parse(db);
 
-        console.log(editTask);
-        console.log(editDate)
+        let changeTask = document.getElementById(`task${num}`);
+        let changeDate = document.getElementById(`date${num}`);
+        
+        if (changeTask.value !== "") {
+            dbParsed[num]["task"] = changeTask.value;
+        } 
+
+        if (changeDate.value !== "") {
+            dbParsed[num]["date"] = changeDate.value;
+        }
+
+        let dbStringify = JSON.stringify(dbParsed);
+        localStorage.setItem("tasks-at-hand", dbStringify);
+        updateTable()
     }
