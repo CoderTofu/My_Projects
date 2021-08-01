087f849dffa3b87d9c0929819da428c60c58e151
tofuLove <vioneverforget@gmail.com>
2021-08-01 11:53:56 +0800

    did some progress record page

---
+++ 07-Voice_Recording/js/main.js
@@ -0,0 +1,32 @@
+// navigator.mediaDevices.getUserMedia({audio: true})
+//     .then(stream => {
+//         const mediaRecorder = new MediaRecorder(stream);
+//         mediaRecorder.start();
+
+//         const audioChunks = [];
+
+//         mediaRecorder.addEventListener('dataavailable', e => {
+//             audioChunks.push(e.data)
+//         });
+
+//         mediaRecorder.addEventListener('stop', () => {
+//             const audioBlob = new Blob(audioChunks);
+//             const audioUrl = URL.createObjectURL(audioBlob);
+//             const audio = new Audio(audioUrl);
+//             audio.play()
+//         })
+
+//         setTimeout(() => {
+//             mediaRecorder.stop();
+//         }, 3000)
+//     })
+
+/*
+
+    Once the user arrives at the record page. He/She will be met with a pre recorded audio(PRA).
+    Pag pinindot ng user ng mic button mawawala yung, PRA at mapapalitan ng magiging audio ni ininput nila.
+
+    SOOOOO pag nasa play mode ang website gagamit tayo ng waves oscillation. Pag nagrerecord naman dapat
+    ang gagamitin is bars.
+
+*/
